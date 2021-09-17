fab7f0c24eefddc1647f719ed17336086fba4e7a
coti8855 <coti8855@gmail.com>
2021-09-17 11:38:10 -0300

    primer commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+var variableuno = prompt ("Ingresar numero");
+var variabledos = prompt ("Ingresar otro numero");
+var suma = 120 + parseInt ( variableuno) + parseInt ( variabledos );
+console.log ( suma );
+var variabletres = prompt ("Ingrese su nombre");
+console.log (variabletres);
+
+var variablecuatro = prompt ("otro numero");
+var multi = 2* parseInt (variablecuatro);
+console.log ("el restulrado es " + multi);
+
+var variablecinco = parseInt (multi) + 40 * parseInt ( variablecuatro)
+console.log (variablecinco)
+
+var variableseis = parseInt (variablecuatro) + parseInt (variableuno) * (multi) - parseInt (variabledos)
+console.log (variableseis)
