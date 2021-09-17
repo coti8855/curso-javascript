38b62dc3c80b178f2dfb37c1a0c3462614464cfb
coti8855 <coti8855@gmail.com>
2021-09-17 11:42:39 -0300

    otro commit

--- javascript.js
+++ javascript.js
@@ -10,8 +10,4 @@ var variablecuatro = prompt ("otro numero");
 var multi = 2* parseInt (variablecuatro);
 console.log ("el restulrado es " + multi);
 
-var variablecinco = parseInt (multi) + 40 * parseInt ( variablecuatro)
-console.log (variablecinco)
 
-var variableseis = parseInt (variablecuatro) + parseInt (variableuno) * (multi) - parseInt (variabledos)
-console.log (variableseis)
