12201d2f1da5ebdc26abdc1c7327c877857d7432
coti8855 <coti8855@gmail.com>
2021-09-21 20:58:59 -0300

    realice el desafio 2

--- javascript.js
+++ javascript.js
@@ -6,8 +6,29 @@ console.log ( suma );
 var variabletres = prompt ("Ingrese su nombre");
 console.log (variabletres);
 
-var variablecuatro = prompt ("otro numero");
-var multi = 2* parseInt (variablecuatro);
-console.log ("el restulrado es " + multi);
+var edad = prompt ("Ingrese su edad");
+edad= parseInt (edad);
 
+if (edad >= 18) {
+  console.log ("Mayor de edad " + edad);
+}
+ else {
+ 	console.log ("Menor de edad " + edad );
+ }
+
+ var codigo = prompt ("ingrese el codigo");
+ codigo= parseInt (codigo);
+
+if (codigo == 555 || codigo == 444) {
+	console.log ("codigo correcto");
+}
+else if (codigo <=99 ){
+	console.log ("falta un digito");
+}
+else if (codigo > 999){
+	console.log ("hay un digito de mas");
+}
+else{
+	console.log ("codigo incorrecto");
+}
 
