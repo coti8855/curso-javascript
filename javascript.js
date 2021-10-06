7c818420f11a65525dfa47fbe45173c906ea61b9
coti8855 <coti8855@gmail.com>
2021-10-06 19:56:11 -0300

    desafio 5

--- javascript.js
+++ javascript.js
@@ -1,51 +1,142 @@
 
-var variableuno = prompt ("Ingresar numero");
-var variabledos = prompt ("Ingresar otro numero");
-var suma = 120 + parseInt ( variableuno) + parseInt ( variabledos );
-console.log ( suma );
-var variabletres = prompt ("Ingrese su nombre");
-console.log (variabletres);
-
-var edad = prompt ("Ingrese su edad");
-edad= parseInt (edad);
-
-if (edad >= 18) {
-  console.log ("Mayor de edad " + edad);
+function convertirSegundos (minutos) {
+	 let segundos = minutos * 60
+	 console.log ("el resultado en segundos es" + segundos)
 }
- else {
- 	console.log ("Menor de edad " + edad );
+ let minutos = parseInt (prompt ("ingrese cantidad de minutos"))
+ convertirSegundos (minutos)
+
+
+
+
+ let animalesGallinas = parseInt (prompt ("ingrese cuantas gallinas hay"))
+ let patasGallinas = animalesGallinas *2
+ let animalesVacas = parseInt (prompt ("ingrese cuantas vacas hay"))
+ let animalesCerdos = parseInt (prompt ("ingrese cuantos cerdos hay"))
+ let patasVacas = animalesVacas *4
+ let patasCerdos = animalesCerdos *4
+ console.log ("hay un total de" + (patasVacas + patasGallinas + patasCerdos) )
+
+let edadPelicula = parseInt (prompt ("Ingrese su edad")) 
+let acompañadoTutor = prompt ("Va acompañado de un tutor")
+
+if (edadPelicula >= 13||edadPelicula < 13 && acompañadoTutor == "si"||acompañadoTutor == "Si" ){
+	console.log ("puede ver pelicula")
+}
+else if (edadPelicula < 13 && acompañadoTutor == "No"||acompañadoTutor == "no" ){
+  console.log ("no puede ver pelicula")
+}
+
+
+let costoProducto = parseInt (prompt ("Ingrese el costo del producto"))
+console.log ("costo producto " + costoProducto)
+if (costoProducto >= 2000){
+	let cantidadCuotas = parseInt (prompt ("Realizar en 12 o 6 cuotas?"))
+	if (cantidadCuotas == 12 || cantidadCuotas == 6) {
+		console.log ("Se paga en cuotas de " + costoProducto / cantidadCuotas)
+	}
+	else{
+		console.log ("No es valido")
+	}
+}
+else if (costoProducto < 2000){
+	 let conSinCuotas = prompt ("Desea realizar el pago en 6 cuotas?")
+	 if (conSinCuotas == "no" || conSinCuotas == "No"){
+	 	console.log ("pago sin cuotas")
+	 }
+	 else if (conSinCuotas == "si"||conSinCuotas == "Si"){
+	 	console.log ("Se paga en cuotas de " + costoProducto/6)
+	 }
+	 else{
+	  console.log ("No es valido")
+	 }
+}
+
+let totalCompra = parseInt (prompt ("Ingrese el valor total de la compra"));
+let costoEnvio = 500;
+const descuentoEfectivo = a => a-a* 0.2 ;
+const suma = (a,b) => a+b;
+let envioSiNo = prompt ("Envio si/no");
+let efectivoSiNo = prompt ("Realizar pago en efectivo si/no")
+ 
+if (efectivoSiNo == "si"|| efectivoSiNo == "Si" && envioSiNo == "si" || envioSiNo == "Si"){
+	let sumaTotal = suma (costoEnvio, descuentoEfectivo (totalCompra))
+	console.log ("Precio final " + sumaTotal)
+}
+else if (efectivoSiNo == "si"|| efectivoSiNo == "Si" && envioSiNo == "no" || envioSiNo == "No"){
+	let sumaTotal2 = suma (descuentoEfectivo (totalCompra),0)
+	console.log ("Precio final " + sumaTotal2)
+}
+ else if (efectivoSiNo == "No"|| efectivoSiNo == "no" && envioSiNo == "no" || envioSiNo == "No"){
+ 	let sumaTotal3 = suma (0, totalCompra)
+ 	console.log ("Precio final " + sumaTotal3)
  }
-
- var codigo = prompt ("ingrese el codigo");
- codigo= parseInt (codigo);
-
-if (codigo == 555 || codigo == 444) {
-	console.log ("codigo correcto");
+ else if (efectivoSiNo == "no"|| efectivoSiNo == "No" && envioSiNo == "si" || envioSiNo == "Si"){
+	let sumaTotal4 = suma (costoEnvio, totalCompra)
+	console.log ("Precio final " + sumaTotal4)
 }
-else if (codigo <=99 ){
-	console.log ("falta un digito");
+
+let cantidadProductos = parseInt (prompt ("Ingrese cantidad de productos del carrito"))
+console.log (cantidadProductos)
+for (var i = 0; i < cantidadProductos; i++) {
+	 let precioProductos = parseInt (prompt ("ingrese precio de los productos"))
+	 console.log (precioProductos + precioProductos)
+	 
 }
-else if (codigo > 999){
-	console.log ("hay un digito de mas");
+
+let precioProduc = parseInt (prompt ("ingrese un numero"))
+let elEnvio = prompt ("Desea realizar envio? Si/No")
+let plataEnvio = 500;
+function sumatoria (envio){
+if (envio == "Si" || "si"){
+	return true
 }
 else{
-	console.log ("codigo incorrecto");
+	return false
+
 }
-	var i=0;
+}
 
-for (var i = 0 ; i < 5; i++) {
-	let nombreUsuario = prompt ("Ingrese su nombre completo");
-	let voto = prompt ("ingrese su voto");
-	console.log (nombreUsuario)
-	console.log (voto)
+if (sumatoria (elEnvio)){
+	let resultado = precioProduc + plataEnvio;
+	console.log (resultado)
+}
+else {
+		console.log (precioProduc)
+}
+
+
+
+
+
+let numero = parseInt (prompt ("ingresar codigo"))
+let usuario = prompt ("ingrese usuario")
+
+function validarUsuario (usuario){
+  let usuarioRegistrado = "Pepe"
+  if (usuario == usuarioRegistrado){
+  	return true;
+  }else{
+  	return false;
   }
+}
+function validarCodigo (usuario){
+  let codigoRegistrado = "123"
+  if (numero == codigoRegistrado){
+  	return true;
+  }else{
+  	return false;
+  }
+}
+function login (usuario, contra){
+	if(usuario== true && contra ==true){
+		return true
+	}
+}
 
-	let nombreUsuario3 = prompt ("ingrse su nombre de nuevo");
-do{
-  var codigoDeEntrada = prompt ("ingrese su codigo");
-  var nombreUsuario2 = prompt ("ingrse su nombre de nuevo");
+if (login(validarUsuario (usuario)), validarCodigo(numero)){
+	console.log ("Bienvenido al sistema")
+}
 
-  console.log (nombreUsuario2);
-  console.log (codigoDeEntrada);
-} while (nombreUsuario2 != "Salir")
+
 
